eb0f1999b917e9826262b601692cc890b37f36b9
kizzuato <kyyajuara@gmail.com>
2026-04-28 08:47:46 +0700

    adding springboot template start

---
+++ src/main/java/com/example/SiKePang/SiKePangApplication.java
@@ -0,0 +1,13 @@
+package com.example.SiKePang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiKePangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiKePangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SiKePang/SiKePangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SiKePang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiKePangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
