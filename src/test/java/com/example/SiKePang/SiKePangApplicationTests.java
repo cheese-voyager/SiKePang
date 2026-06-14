1bf1643dc0dd09f5aae6fcafb4c9415d1b011791
cheese-voyager <zakhwaaliyamaryam78@gmail.com>
2026-06-14 10:13:18 +0000

    Fix syntax error pada unit test

--- src/test/java/com/example/SiKePang/SiKePangApplicationTests.java
+++ src/test/java/com/example/SiKePang/SiKePangApplicationTests.java
@@ -11,16 +11,3 @@ class SiKePangApplicationTests {
 	}
 
 }
-package com.example.SiKePang;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SiKePangApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
